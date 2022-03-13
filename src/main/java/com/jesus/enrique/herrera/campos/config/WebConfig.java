48b94269f51c8aaf5f2f903e090bbd0106cc8067
utn203241034 <93666188+utn203241034@users.noreply.github.com>
2022-03-13 14:21:18 -0600

    Update WebConfig.java

--- src/main/java/com/jesus/enrique/herrera/campos/config/WebConfig.java
+++ src/main/java/com/jesus/enrique/herrera/campos/config/WebConfig.java
@@ -7,6 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/logos/**").addResourceLocations("file:.\\empleosjehc\\imagenes\\");
+		registry.addResourceHandler("/logos/**").addResourceLocations("file:C:\\empleosjehc\\imagenes\\");
 	}
 }
